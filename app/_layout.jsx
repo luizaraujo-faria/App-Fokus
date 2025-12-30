5ddc928d28f6e8dccf1e45da83948bc07acd9fa0
luiz <luiz308araujofarias@gmail.com>
2025-12-30 16:57:11 -0300

    style: Tela Inicial Pronta

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -3,6 +3,7 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack screenOptions={{ title: 'Fokus' }}>
+      <Stack.Screen name='index' options={{ headerShown: false }} />
       <Stack.Screen name='pomodoro' options={{ headerShown: false }} />
     </Stack>
   );
--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,61 @@
 import { Link } from "expo-router";
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
+import { FokusButton } from '../components/FokusButton';
 
 export default function Index(){
 
   return(
-    <View>
-      <Text>Home</Text>
-      <Link href={{ pathname: '/pomodoro' }}>Quero iniciar</Link>
+    <View style={styles.container}>
+      {/* <Text>Home</Text>
+      <Link href={{ pathname: '/pomodoro' }}>Quero iniciar</Link> */}
+      <Image 
+        source={require('../assets/images/Logo.png')} />
+      
+      <View style={styles.inner}>
+        <Text style={styles.title}>
+          Otimize sua {'\n'} produtividade, {'\n'}
+          <Text style={styles.bold}>
+            mergulhe no que {'\n'} importa
+          </Text> 
+        </Text> 
+        <Image 
+          source={require('../assets/images/Imagem tela inicial.png')} />
+        <FokusButton title='Quero iniciar!' />
+      </View>
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>Projeto Fictício Sem Fina Comerciais.</Text>
+        <Text style={styles.footerText}>Desenvolvido por Alura.</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#021123',
+    gap: 40
+  },
+  inner: {
+    gap: 16
+  },
+  title: {
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 26,
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+  footer: {
+    width: '80%'
+  },
+  footerText: {
+    textAlign: 'center',
+    color: '#98a0a8',
+    fontSize: 12.5,
+  },
+});
